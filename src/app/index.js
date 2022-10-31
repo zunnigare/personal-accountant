265fc134d3ce1773dccc08d0f6e55eb194da0be5
Ricardo Zúñiga <litosiderito@gmail.com>
2022-10-31 17:42:43 -0600

    Cambios

--- src/app/index.js
+++ src/app/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import {StrictMode} from 'react';
 import App from './App';
 
+//RootAPP
 const root = ReactDOM.createRoot(document.getElementById("app"));
 
 root.render(<StrictMode>

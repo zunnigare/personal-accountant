0dfd3286690d5b2b93cce92d29671d270674c7f8
Ricardo Zúñiga <litosiderito@gmail.com>
2022-11-03 13:26:42 -0600

    Prod server

--- src/app-dev.js
+++ src/app-dev.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 //CONFIGURACIONES
 app.set('PORT', process.env.PORT || 3000);
---
+++ src/app.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');

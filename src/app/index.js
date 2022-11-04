028492409385e0e98b04fb7c3ee857e7f3350f11
Ricardo Zúñiga <litosiderito@gmail.com>
2022-11-04 14:56:06 -0600

    Formato cambios

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
-import {StrictMode} from 'react';
+import { StrictMode } from 'react';
 import App from './App';
 
 //RootAPP
 const root = ReactDOM.createRoot(document.getElementById("app"));
 
 root.render(<StrictMode>
-    <App />
-  </StrictMode>);
+  <App />
+</StrictMode>);

ab0bc0aa50a98bed39103362d9911a9302e97058
Ricardo Zúñiga <litosiderito@gmail.com>
2022-11-05 08:53:27 -0600

    configs

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -6,3 +7,5 @@ const bodyParser = require('body-parser');
 
 //CONFIGURACIONES
 app.set('PORT', process.env.PORT || 3000);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
